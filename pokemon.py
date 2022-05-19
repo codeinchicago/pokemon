cedf0976c69628d35307fea28b300b2097f97342
codeinchicago <vancewcole@gmail.com>
2022-05-19 07:41:19 -0500

    Minor text output improvements.

--- pokemon.py
+++ pokemon.py
@@ -3,19 +3,22 @@ Pokemon API Wrapper
 Create a python wrapper for the Pokemon API. It should take in a pokemon name and display the pokemon with its height and weight
 """
 
-#To do:
-# Deal with input of wrong Pokemon name/errors in input.
-
 import requests
 
-def pokefinder(rodent):
+rodent = input("What pokemon would you like to investigate? ").lower()
+
+def pokefinder():
+
     lookup = requests.get(f"https://pokeapi.co/api/v2/pokemon/{rodent}")
+
+    if lookup.status_code == 200:
+        print("Pokemon found, measuring it.")
 
     data = lookup.json()
     height = data['height']
-    print(f"This pokemon is {height} centimeters tall.")
+    print(f"{rodent.title()} is {height} centimeters tall.")
 
     weight = data['weight']
-    print(f"This pokemon weighs {weight} grams.")
+    print(f"{rodent.title()} weighs {weight} grams.")
 
-pokefinder("squirtle")
+pokefinder()
